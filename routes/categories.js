6120979a511ddfc31fc53091439c8d6d5ea0171e
Mike <mbrie041@gmail.com>
2021-04-05 18:15:42 +0000

    implement category and login routes.

---
+++ routes/categories.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+  //displays apps page
+  router.get("/apps", (req, res) => {
+    db.query(`SELECT * FROM products WHERE isApp IS TRUE;`)
+      .then(data => {
+        const apps = data.rows;
+        res.json({ apps });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  //displays dinner page
+  router.get("/dinner", (req, res) => {
+    db.query(`SELECT * FROM products WHERE isDinner IS TRUE;`)
+      .then(data => {
+        const dinners = data.rows;
+        res.json({ dinners });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  //displays desserts page
+  router.get("/desserts", (req, res) => {
+    db.query(`SELECT * FROM products WHERE isDessert IS TRUE;`)
+      .then(data => {
+        const desserts = data.rows;
+        res.json({ desserts });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  //displays drinks page
+  router.get("/drinks", (req, res) => {
+    db.query(`SELECT * FROM products WHERE isDrink IS TRUE;`)
+      .then(data => {
+        const drinks = data.rows;
+        res.json({ drinks });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
+
--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,68 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
+const { response } = require("express");
+const express = require("express");
 const router = express.Router();
+// const bcrypt = require('bcrypt');
+
+
+
+
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+  router.get("/login", (req, res) => {
+    res.send("Login page");
   });
+
+  const getUserWithEmail = function (email) {
+    return db
+      .query(
+        `
+        SELECT * FROM users
+        WHERE email = $1`,
+        [email.toLowerCase()]
+      )
+
+      .then((res) => {
+        console.log(res.rows);
+        let user = res.rows[0] || null;
+        return user;
+      });
+  };
+  exports.getUserWithEmail = getUserWithEmail;
+
+
+  const login = function (email, password) {
+    return getUserWithEmail(email).then((user) => {
+      if (password === user.password) {
+        return user;
+      }
+      return null;
+    });
+  };
+  exports.login = login;
+
+  router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    login(email, password)
+      .then(user => {
+        if (!user) {
+          res.send({error: "error"});
+          return;
+        }
+        if (email === "admin@summer.com") {
+          req.session.userId = user.id;
+          res.send("redirect to dashboard");
+          return;
+        }
+
+        req.session.userId = user.id;
+        res.send("we're going to redirect here");
+      })
+      .catch(e => res.send(e));
+  });
+
+
   return router;
 };
