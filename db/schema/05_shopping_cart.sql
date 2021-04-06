dc332983c891ec584674390eede6f7f1c86bcd6c
Mike <mbrie041@gmail.com>
2021-04-06 23:48:13 +0000

    add working decrement route

--- db/schema/05_shopping_cart.sql
+++ db/schema/05_shopping_cart.sql
@@ -4,5 +4,6 @@ CREATE TABLE shopping_cart (
   product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   quantity INTEGER NOT NULL,
+     constraint quantity_nonnegative check (quantity >= 0),
   UNIQUE(product_id, user_id)
 );
--- routes/products.js
+++ routes/products.js
@@ -3,35 +3,48 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.post("/:id/shopping-cart-increment", (req, res) => {
-    console.log("req.session", req.session)
-
-
     db.query(
       `
       INSERT INTO shopping_cart(user_id, product_id, quantity)
       VALUES(${req.session.userId}, ${req.params.id}, 1)
       ON CONFLICT(product_id, user_id) DO UPDATE
-      SET quantity = shopping_cart.quantity +1
+      SET quantity = shopping_cart.quantity + 1
       WHERE shopping_cart.user_id = ${req.session.userId}
       AND shopping_cart.product_id = ${req.params.id};`
-    )
-      .then(res => console.log(res));
+    ).then(() => (res.status(200).end()))
+  });
+
+  router.post("/:id/shopping-cart-decrement", (req, res) => {
+    const firstQuery =  `SELECT * FROM shopping_cart
+    WHERE user_id = ${req.session.userId}
+    AND product_id = ${req.params.id}
+    ;`
+    console.log(firstQuery);
+    return db.query(firstQuery)
+    .then((data) => {
+      if(!data.rows[0]){
+        return;
+      }
+      if (data.rows[0].quantity > 1) {
+        const queryString = `
+        UPDATE shopping_cart
+        SET quantity = shopping_cart.quantity - 1
+        WHERE user_id = ${req.session.userId}
+        AND product_id = ${req.params.id};
+        `;
+        console.log(queryString);
+        return db.query(queryString);
+      }
+      if ((data.rows[0].quantity === 1)) {
+        return db.query(`
+          DELETE FROM shopping_cart
+          WHERE user_id = ${req.session.userId}
+          AND product_id = ${req.params.id};
+          `);
+      }
+
+    }).then(() => (res.status(200).end()))
+    .catch(e=>(console.log(e)))
   });
   return router;
-  //INSERT INTO shopping_cart (user_id, product_id, quantity) VALUES (1, 1, 1) ON CONFLICT(product_id, user_id) DO UPDATE SET quantity = shopping_cart.quantity +1;
-
-  // return quantity;
-  //return new value
-  // router.post("/products/:id/shopping-cart-decrement", (req, res) => {
-  //   db.query(`
-  //     INSERT INTO shopping_cart(user_id, product_id, quantity)
-  //     VALUES(req.session.user_id, req.params.id, 1)
-  //     ON CONFLICT DO UPDATE
-  //     SET quantity = quantity -1
-  //     WHERE user_id = req.session.user_id
-  //     AND product_id = req.params.id;`);
-
-  //   return quantity;
-  //   //return new value
-  // });
 };
