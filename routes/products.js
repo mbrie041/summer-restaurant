ed7a00ce5a876db8c8c2c52237ddf83460e1def4
Mike <mbrie041@gmail.com>
2021-04-06 13:28:42 +0000

    complete changes to products and database for + & -

--- routes/products.js
+++ routes/products.js
@@ -1,25 +1,30 @@
-router.get("/products/:id/shopping-cart-increment", (req, res) => {
-INSERT INTO shopping_cart(user_id, product_id, quantity)
-VALUES(req.session.user_id, req.params.id, 1)
-ON CONFLICT DO UPDATE
-SET quantity = quantity + 1
-WHERE user_id = req.session.user_id
-AND product_id = req.params.id;
+const express = require("express");
+const router = express.Router();
 
+module.exports = (db) => {
+  router.get("/products/:id/shopping-cart-increment", (req, res) => {
+    db.query(`
+      INSERT INTO shopping_cart(user_id, product_id, quantity)
+      VALUES(req.session.user_id, req.params.id, 1)
+      ON CONFLICT DO UPDATE
+      SET quantity = quantity + 1
+      WHERE user_id = req.session.user_id
+      AND product_id = req.params.id;`);
 
-return quantity;
-//return new value
-})
+    return quantity;
+    //return new value
+  });
 
-router.get("/products/:id/shopping-cart-decrement", (req, res) => {
-  INSERT INTO shopping_cart(user_id, product_id, quantity)
-  VALUES(req.session.user_id, req.params.id, 1)
-  ON CONFLICT DO UPDATE
-  SET quantity = quantity -1
-  WHERE user_id = req.session.user_id
-  AND product_id = req.params.id;
+  router.get("/products/:id/shopping-cart-decrement", (req, res) => {
+    db.query(`
+      INSERT INTO shopping_cart(user_id, product_id, quantity)
+      VALUES(req.session.user_id, req.params.id, 1)
+      ON CONFLICT DO UPDATE
+      SET quantity = quantity -1
+      WHERE user_id = req.session.user_id
+      AND product_id = req.params.id;`);
 
-
-  return quantity;
-  //return new value
-  })
+    return quantity;
+    //return new value
+  });
+};
