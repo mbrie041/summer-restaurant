140d947f616ca63cad62e86e62634c96b1ae987b
Mike <mbrie041@gmail.com>
2021-04-06 19:50:01 +0000

    update increment and decrement calls to work

--- routes/categories.js
+++ routes/categories.js
@@ -1,70 +1,64 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const getAllFood = function(db) {
-  return db.query(`SELECT * FROM products WHERE isApp IS TRUE;`)
-  .then(data => {
-    const apps = data.rows;
-    return apps;
-  })
+const getAllFood = function (db) {
+  return db
+    .query(`SELECT * FROM products WHERE isApp IS TRUE;`)
+    .then((data) => {
+      const apps = data.rows;
+      return apps;
+    });
 };
 
 module.exports = (db) => {
   //displays apps page
   router.get("/apps", (req, res) => {
-    return getAllFood(db).then(function(resolvedFoods){
-      return res.send({foods: resolvedFoods});
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
+    return getAllFood(db)
+      .then(function (resolvedFoods) {
+        return res.render("food-menu", { foods: resolvedFoods });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
   });
   //displays dinner page
   router.get("/dinner", (req, res) => {
     db.query(`SELECT * FROM products WHERE isDinner IS TRUE;`)
-      .then(data => {
+      .then((data) => {
         const dinners = data.rows;
         res.json({ dinners });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
-      res.render('food-menu');
+    res.render("food-menu");
   });
 
   //displays desserts page
   router.get("/desserts", (req, res) => {
     db.query(`SELECT * FROM products WHERE isDessert IS TRUE;`)
-      .then(data => {
+      .then((data) => {
         const desserts = data.rows;
         res.json({ desserts });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
-      res.render('food-menu');
+    res.render("food-menu");
   });
 
   //displays drinks page
   router.get("/drinks", (req, res) => {
     db.query(`SELECT * FROM products WHERE isDrink IS TRUE;`)
-      .then(data => {
+      .then((data) => {
         const drinks = data.rows;
         res.json({ drinks });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
-      res.render('food-menu');
+    res.render("food-menu");
   });
+
   return router;
 };
-
--- routes/products.js
+++ routes/products.js
@@ -2,29 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get("/products/:id/shopping-cart-increment", (req, res) => {
-    db.query(`
+  router.post("/:id/shopping-cart-increment", (req, res) => {
+    db.query(
+      `
       INSERT INTO shopping_cart(user_id, product_id, quantity)
-      VALUES(req.session.user_id, req.params.id, 1)
-      ON CONFLICT DO UPDATE
-      SET quantity = quantity + 1
-      WHERE user_id = req.session.user_id
-      AND product_id = req.params.id;`);
-
-    return quantity;
-    //return new value
+      VALUES(${req.session.user_id}, ${req.params.id}, 1)
+      ON CONFLICT(product_id, user_id) DO UPDATE
+      SET quantity = shopping_cart.quantity +1
+      WHERE user_id = ${req.session.user_id}
+      AND product_id = ${req.params.id};`
+    ).then((res) => console.log(res));
   });
+  return router;
+  //INSERT INTO shopping_cart (user_id, product_id, quantity) VALUES (1, 1, 1) ON CONFLICT(product_id, user_id) DO UPDATE SET quantity = shopping_cart.quantity +1;
 
-  router.get("/products/:id/shopping-cart-decrement", (req, res) => {
-    db.query(`
-      INSERT INTO shopping_cart(user_id, product_id, quantity)
-      VALUES(req.session.user_id, req.params.id, 1)
-      ON CONFLICT DO UPDATE
-      SET quantity = quantity -1
-      WHERE user_id = req.session.user_id
-      AND product_id = req.params.id;`);
+  // return quantity;
+  //return new value
+  // router.post("/products/:id/shopping-cart-decrement", (req, res) => {
+  //   db.query(`
+  //     INSERT INTO shopping_cart(user_id, product_id, quantity)
+  //     VALUES(req.session.user_id, req.params.id, 1)
+  //     ON CONFLICT DO UPDATE
+  //     SET quantity = quantity -1
+  //     WHERE user_id = req.session.user_id
+  //     AND product_id = req.params.id;`);
 
-    return quantity;
-    //return new value
-  });
+  //   return quantity;
+  //   //return new value
+  // });
 };
