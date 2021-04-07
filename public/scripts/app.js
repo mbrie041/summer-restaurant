f3eb97c2fb082e57340290713084bf05a8c70708
Mike <mbrie041@gmail.com>
2021-04-07 22:05:04 +0000

    increment display total

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,8 +3,9 @@ $(document).ready(function () {
     const userID = $('#hidden_userID').val();
     if(userID){
     console.log("event target value",event.currentTarget.value);
-  $.post(`/api/products/${event.currentTarget.value}/shopping-cart-increment`).then(() => {
-    console.log("post was done");
+  $.post(`/api/products/${event.currentTarget.value}/shopping-cart-increment`).then((results) => {
+    console.log("post was done", results);
+    $(this).siblings(".textclass").html(results.results.quantity)
    });
   } else {
     window.location.replace("/api/users/login?");
@@ -17,6 +18,7 @@ $(document).ready(function () {
     console.log("event target value",event.currentTarget.value);
   $.post(`/api/products/${event.currentTarget.value}/shopping-cart-decrement`).then(() => {
     console.log("post was done");
+    // $(this).siblings(".textclass").html(results.results.quantity)
    });
   } else {
     window.location.replace("/api/users/login?");
@@ -26,3 +28,7 @@ $(document).ready(function () {
 
 
 
+// SELECT products.*, shopping_cart.quantity as cart_quantity
+// FROM products
+// JOIN shopping_cart ON products.id = shopping_cart.product_id
+// WHERE isApp IS TRUE;
--- routes/products.js
+++ routes/products.js
@@ -10,42 +10,87 @@ module.exports = (db) => {
       ON CONFLICT(product_id, user_id) DO UPDATE
       SET quantity = shopping_cart.quantity + 1
       WHERE shopping_cart.user_id = ${req.session.userId}
-      AND shopping_cart.product_id = ${req.params.id};`
-    ).then(() => res.status(200).end());
+      AND shopping_cart.product_id = ${req.params.id}
+      RETURNING *;`
+    ).then((data) => {
+      // console.log("returned data>>>", data);
+      let results = data.rows[0];
+      res.status(200).send({ results });
+    });
   });
 
-  router.post("/:id/shopping-cart-decrement", (req, res) => {
-    const firstQuery = `SELECT * FROM shopping_cart
-    WHERE user_id = ${req.session.userId}
-    AND product_id = ${req.params.id}
-    ;`;
-    console.log(firstQuery);
-    return db
-      .query(firstQuery)
-      .then((data) => {
-        if (!data.rows[0]) {
-          return;
-        }
-        if (data.rows[0].quantity > 1) {
-          const queryString = `
-        UPDATE shopping_cart
-        SET quantity = shopping_cart.quantity - 1
+//   router.post("/:id/shopping-cart-decrement", (req, res) => {
+//     const firstQuery = `SELECT * FROM shopping_cart
+//     WHERE user_id = ${req.session.userId}
+//     AND product_id = ${req.params.id}
+//     ;`;
+//     // console.log(firstQuery);
+//     return db
+//       .query(firstQuery)
+//       .then((data) => {
+//         if (!data.rows[0]) {
+//           return;
+//         }
+//         if (data.rows[0].quantity > 1) {
+//           const queryString = `
+//         UPDATE shopping_cart
+//         SET quantity = shopping_cart.quantity - 1
+//         WHERE user_id = ${req.session.userId}
+//         AND product_id = ${req.params.id};
+//         `;
+//           console.log("Query string>>>>",queryString);
+//           db.query(queryString);
+//         }
+//         if (data.rows[0].quantity === 1) {
+//           db.query(`
+//           DELETE FROM shopping_cart
+//           WHERE user_id = ${req.session.userId}
+//           AND product_id = ${req.params.id};
+//           `);
+//         }
+//       })
+//       .then((data) => {
+//         console.log("returned data>>>", data);
+//         let results = data.rows[0];
+//         res.status(200).send({ results })
+//       })
+//       .catch((e) => console.log(e));
+//   });
+//   return router;
+// };
+
+router.post("/:id/shopping-cart-decrement", (req, res) => {
+  const firstQuery = `SELECT * FROM shopping_cart
+  WHERE user_id = ${req.session.userId}
+  AND product_id = ${req.params.id}
+  ;`;
+  console.log(firstQuery);
+  return db
+    .query(firstQuery)
+    .then((data) => {
+      if (!data.rows[0]) {
+        return;
+      }
+      if (data.rows[0].quantity > 1) {
+        const queryString = `
+      UPDATE shopping_cart
+      SET quantity = shopping_cart.quantity - 1
+      WHERE user_id = ${req.session.userId}
+      AND product_id = ${req.params.id};
+      `;
+        console.log(queryString);
+        return db.query(queryString);
+      }
+      if (data.rows[0].quantity === 1) {
+        return db.query(`
+        DELETE FROM shopping_cart
         WHERE user_id = ${req.session.userId}
         AND product_id = ${req.params.id};
-        `;
-          console.log(queryString);
-          return db.query(queryString);
-        }
-        if (data.rows[0].quantity === 1) {
-          return db.query(`
-          DELETE FROM shopping_cart
-          WHERE user_id = ${req.session.userId}
-          AND product_id = ${req.params.id};
-          `);
-        }
-      })
-      .then(() => res.status(200).end())
-      .catch((e) => console.log(e));
-  });
-  return router;
+        `);
+      }
+    })
+    .then(() => res.status(200).end())
+    .catch((e) => console.log(e));
+});
+return router;
 };
