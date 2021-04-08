659060834ab98ea5540bde69fed6e536b5c1647b
Mike <mbrie041@gmail.com>
2021-04-08 00:05:08 +0000

    merge wednesdays work from group

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, phone_number, email, password) VALUES ('admin', '+1(416)458-8333', 'admin@summer.com', 'test');
-INSERT INTO users (name, phone_number, email, password) VALUES ('Lucas Pinheiro', '+1(416)859-7425', 'lucas@gmail.com', 'test');
+INSERT INTO users (name, phone_number, email, password) VALUES ('admin', '+5592984032212', 'admin@summer.com', 'test');
+INSERT INTO users (name, phone_number, email, password) VALUES ('Lucas Pinheiro', '+5592984032212', 'lucas@gmail.com', 'test');
 
 
 
